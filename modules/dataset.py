48b20fe24f65838090ad44ed06c0c57a6c34a15c
Mike <mikhail.papkov@gmail.com>
2020-09-02 12:43:58 +0300

    Add dataset caching and train/val augmentations

--- modules/dataset.py
+++ modules/dataset.py
@@ -1,6 +1,4 @@
 import glob
-import os
-import sys
 from pathlib import Path
 from typing import List, Tuple, Union
 
@@ -10,8 +8,6 @@ import pandas as pd
 from albumentations.pytorch import transforms as T
 from torch import Tensor
 from torch.utils.data import Dataset, Sampler, SubsetRandomSampler
-
-# from torchvision import transforms as T
 
 
 def prepare_metainfo_labels(metainfo: pd.DataFrame) -> pd.DataFrame:
@@ -50,11 +46,13 @@ def prepare_metainfo_labels(metainfo: pd.DataFrame) -> pd.DataFrame:
 class FramesDataset(Dataset):
     def __init__(
         self,
+        path_data: str = "data",
+        dir_frames: str = "frames",
+        file_metainfo: str = "metainfo.csv",
         replicate: Union[str, int] = "*",
-        path: str = "./data/frames_separated/",
         fmt: str = "npy",
         validation_field: Union[int, None] = 4,
-        select: Union[str, List[str]] = "wt",
+        select: Union[None, str, List[str]] = "wt",
         seed: int = 56,
         transforms: List[A.BasicTransform] = [],
     ):
@@ -66,45 +64,59 @@ class FramesDataset(Dataset):
         assert str(replicate) in ("*", "1", "2", "3")
 
         # Store variables
-        self.path = Path(path)
+        self.path_data = Path(path_data)
+        self.path_frames = self.path_data / dir_frames
+        self.path_metainfo = self.path_data / file_metainfo
+
+        assert self.path_frames.exists()
+        assert self.path_metainfo.exists()
+
         self.replicate = replicate
         self.fmt = fmt
         self.validation_field = validation_field
         self.select = select
+        self.metainfo_cached = self.path_data / f'metainfo_replicate{self.replicate}_{self.select}.csv'
 
         # Read file list
-        self.files = sorted(
-            glob.glob(str(self.path / f"replicate{replicate}" / "*" / f"*.{fmt}"))
-        )
-
-        # Create default metainfo
-        # >>> self.metainfo.head()
-        #         URL     replicate       cell_id       file
-        # 0     0000000       1              0       path/to/file
-        self.metainfo = pd.DataFrame(
-            [np.array(f.split("/"))[[-2, 2, -1]] for f in self.files],
-            columns=["URL", "replicate", "cell_id"],
-        )
-        self.metainfo["file"] = self.files
-
-        # Read metainfo about GFP tags
-        metainfo_gfp = pd.read_csv("./data/metainfo.csv", index_col=0, dtype="str")
-
-        # Select subset by parameters
-        if select is None:
-            pass
-        elif select == "wt":
-            metainfo_gfp = metainfo_gfp[metainfo_gfp.natMX4.isna()]
+        if self.metainfo_cached.exists():
+            print(f'Read cached metainfo from {self.metainfo_cached}')
+            self.metainfo = pd.read_csv(self.metainfo_cached, index_col=0)
+            self.files = self.metainfo["file"]
         else:
-            metainfo_gfp = metainfo_gfp[metainfo_gfp.GFP.isin(select)]
+            self.files = sorted(
+                glob.glob(str(self.path_frames / f"replicate{replicate}" / "*" / f"*.{fmt}"))
+            )
+            # Create default metainfo
+            # >>> self.metainfo.head()
+            #         URL     replicate       cell_id       file
+            # 0     0000000       1              0       path/to/file
+            self.metainfo = pd.DataFrame(
+                [np.array(f.split("/"))[[-2, 2, -1]] for f in self.files],
+                columns=["URL", "replicate", "cell_id"],
+            )
+            self.metainfo["file"] = self.files
 
-        # Prepare labels and classes
-        metainfo_gfp = prepare_metainfo_labels(metainfo_gfp)
-        self.n_classes = metainfo_gfp.GFP.cat.codes.max() + 1
+            # Read metainfo about GFP tags
+            metainfo_gfp = pd.read_csv(self.path_metainfo, index_col=0, dtype="str")
 
-        # Merge info by URL
-        self.metainfo = self.metainfo[self.metainfo.URL.isin(metainfo_gfp.URL.unique())]
-        self.metainfo = self.metainfo.merge(metainfo_gfp, how="left", on="URL")
+            # Select subset by parameters
+            if select is None:
+                pass
+            elif select == "wt":
+                metainfo_gfp = metainfo_gfp[metainfo_gfp.natMX4.isna()]
+            else:
+                metainfo_gfp = metainfo_gfp[metainfo_gfp.GFP.isin(select)]
+
+            # Prepare labels and classes
+            metainfo_gfp = prepare_metainfo_labels(metainfo_gfp)
+            self.n_classes = metainfo_gfp.GFP.cat.codes.max() + 1
+
+            # Merge info by URL
+            self.metainfo = self.metainfo[self.metainfo.URL.isin(metainfo_gfp.URL.unique())]
+            self.metainfo = self.metainfo.merge(metainfo_gfp, how="left", on="URL")
+
+            print(f'Save cached metainfo to {self.metainfo_cached}')
+            self.metainfo.to_csv(self.metainfo_cached)
 
         # Hold out validation
         if validation_field is None:
@@ -118,11 +130,13 @@ class FramesDataset(Dataset):
             self.valid_indices = np.where(from_validation_field)[0]
             np.random.shuffle(self.train_indices)
 
-        self.transforms = A.Compose([*transforms, T.ToTensorV2()])
+        self.base_transforms = A.Compose([T.ToTensorV2(), ])
+        self.aug_transforms = A.Compose(transforms)
+        self.transforms = A.Compose([self.aug_transforms, self.base_transforms])
 
     def __getitem__(self, i) -> Tuple[Tensor, int]:
         f, cls = self.metainfo.iloc[i][["file", "class"]]
-        img = np.load(f).astype(np.float32)
+        img = np.load(f).astype(np.float32)[..., None]
         cls = int(cls)
 
         # normalize each image
@@ -130,7 +144,7 @@ class FramesDataset(Dataset):
         #         img = np.log(img + 1)
         img -= img.mean()
         img /= img.std()
-        img = self.transforms(img)
+        img = self.transforms(image=img)['image']
 
         return img, cls
 
@@ -144,3 +158,9 @@ class FramesDataset(Dataset):
         train_sampler = SubsetRandomSampler(self.train_indices)
         valid_sampler = SubsetRandomSampler(self.valid_indices)
         return train_sampler, valid_sampler
+
+    def eval(self):
+        self.transforms = A.Compose([self.base_transforms])
+
+    def train(self):
+        self.transforms = A.Compose([self.aug_transforms, self.base_transforms])
---
+++ notebooks/README.md
@@ -0,0 +1,1 @@
+Jupyter notebooks for visualization and interactive testing
