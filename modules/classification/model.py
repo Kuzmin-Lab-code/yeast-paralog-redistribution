253282c4864cd2457390e40e2cfec253b3d5597a
papkov <mikhail.papkov@gmail.com>
2022-07-13 12:25:40 +0300

    Add classification training

--- modules/classification/model.py
+++ modules/classification/model.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pytorch_lightning as pl
 import torch
-from pytorch_lightning.metrics import Accuracy
 from torch import nn
+from torchmetrics import Accuracy
 from tqdm.auto import tqdm
 
 from modules.classification.loss import ArcFaceLoss, ArcMarginProductPlain
@@ -18,20 +18,29 @@ class LitModel(pl.LightningModule):
         metric_coefficient: float = 0.25,
         scale_factor: float = 1.0,
         seed: int = 15,
+        epochs: int = 200,
+        lr: float = 3e-4,
+        min_lr: float = 1e-7,
     ):
         super().__init__()
         np.random.seed(seed)
 
         self.network = network
         self.criterion = criterion
-        self.metric_criterion = metric_criterion
-        self.metric = ArcMarginProductPlain(
-            self.network.fc.in_features, self.network.fc.out_features
-        )
+
         self.metric_coefficient = metric_coefficient
+        if self.metric_coefficient > 0:
+            self.metric_criterion = metric_criterion
+            self.metric = ArcMarginProductPlain(
+                self.network.fc.in_features, self.network.fc.out_features
+            )
         # Metrics
-        self.accuracy = Accuracy(num_classes=self.network.n_classes)
+        self.accuracy = Accuracy()
         self.scale_factor = scale_factor
+
+        self.epochs = epochs
+        self.lr = lr
+        self.min_lr = min_lr
 
     def forward(self, x):
         return self.network(x)
@@ -46,10 +55,11 @@ class LitModel(pl.LightningModule):
 
         features = self.network.features(x)
         out = self.network.fc(features)
-        metric_out = self.metric(features)
         clf_loss = self.criterion(out, y)
 
+        result = {}
         if self.metric_coefficient > 0:
+            metric_out = self.metric(features)
             metric_loss = self.metric_criterion(metric_out, y)
 
             loss = (
@@ -57,17 +67,15 @@ class LitModel(pl.LightningModule):
                 + (1 - self.metric_coefficient) * clf_loss
             )
 
-            result = pl.TrainResult(minimize=loss)
-            result.log("clf_loss", clf_loss)
-            result.log("metric_loss", metric_loss)
+            result["clf_loss"] = clf_loss
+            result["metric_loss"] = metric_loss
         else:
             loss = clf_loss
-            result = pl.TrainResult(minimize=loss)
 
         acc = self.accuracy(out.argmax(1), y)
-        result.log("train_loss", loss)
-        result.log("train_acc", acc)
-        #         result.log('progress_bar', {'acc': acc})
+        result["loss"] = loss
+        result["acc"] = acc
+        self.log_dict(result, prog_bar=True, on_epoch=True)
 
         return result
 
@@ -83,22 +91,23 @@ class LitModel(pl.LightningModule):
         loss = self.criterion(out, y)
         acc = self.accuracy(out.argmax(1), y)
 
-        result = pl.EvalResult(checkpoint_on=loss)
-        result.log_dict(
-            {"val_loss": loss, "val_acc": acc}, prog_bar=True, on_epoch=True
-        )
+        result = {"val_loss": loss, "val_acc": acc}
+        self.log_dict(result, prog_bar=True, on_epoch=True)
 
         return result
 
     def configure_optimizers(self):
-        optimizers = [torch.optim.Adam(self.network.parameters(), lr=3e-4)]
-        # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=50)
+        optimizers = [torch.optim.Adam(self.network.parameters(), lr=self.lr)]
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+            optimizers[0], T_max=self.epochs, eta_min=self.min_lr
+        )
         schedulers = [
             {
-                "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
-                    optimizers[0], factor=0.1
-                ),
-                "monitor": "val_checkpoint_on",  # Default: val_loss
+                # "scheduler": torch.optim.lr_scheduler.ReduceLROnPlateau(
+                #     optimizers[0], factor=0.1
+                # ),
+                "scheduler": scheduler,
+                "monitor": "val_loss",  # Default: val_loss
                 "interval": "epoch",
                 "frequency": 1,
             }
@@ -114,9 +123,11 @@ class LitModel(pl.LightningModule):
 
     def on_train_epoch_start(self):
         try:
-            self.trainer.train_dataloader.dataset.train()
+            # self.trainer.train_dataloader.dataset.train()
+            self.trainer.train_dataloader.loaders.dataset.train()
         except AttributeError:
-            self.trainer.train_dataloader.dataset.dataset.train()
+            # self.trainer.train_dataloader.dataset.dataset.train()
+            self.trainer.train_dataloader.loaders.dataset.dataset.train()
 
     def inference(self, loader):
         self.freeze()
@@ -141,7 +152,7 @@ class LitModel(pl.LightningModule):
                 fts = self.network.features(x.cuda())
                 out = self.network.fc(fts)
 
-                acc.append(self.accuracy(out.argmax(1), y.cuda()))
+                acc.append(self.accuracy(out.argmax(1).cpu(), y).cpu().numpy())
                 iterator.set_postfix({"acc": np.mean(acc) * 100})
 
                 predictions.append(out.cpu().numpy())
---
+++ train_classification.py
@@ -0,0 +1,105 @@
+import os
+
+import albumentations as A
+import hydra
+from hydra.utils import get_original_cwd
+from omegaconf import DictConfig, OmegaConf
+from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
+from torch.utils.data import DataLoader
+
+from modules.classification import dataset, model, network
+from modules.tools import metrics, util
+from modules.tools.types import *
+
+
+@hydra.main(config_path="config", config_name="classification.yaml")
+def main(cfg: DictConfig) -> None:
+    print(OmegaConf.to_yaml(cfg))
+    os.environ["CUDA_VISIBLE_DEVICES"] = f"{cfg.device}"
+
+    cwd = Path(get_original_cwd())
+    util.fix_seed(cfg.seed)
+    print(f"Run from {cwd.as_posix()}, cwd {os.getcwd()}")
+
+    transforms = A.Compose(
+        [
+            A.HorizontalFlip(),
+            A.VerticalFlip(),
+            A.RandomRotate90(),
+            A.ShiftScaleRotate(),
+            # A.RandomBrightnessContrast(),
+            # A.RandomGamma(),
+            # A.Blur(),
+            # A.GaussNoise(var_limit=0.01),
+        ]
+    )
+
+    wt_dataset = dataset.FramesDataset(
+        path_data=cwd / cfg.data.path_data,
+        dir_frames=cfg.data.dir_frames,
+        select="wt",
+        validation_field=cfg.data.validation_field,
+        transforms=transforms,
+        seed=cfg.seed,
+        normalize=cfg.data.normalize,
+    )
+
+    train_sampler, valid_sampler = wt_dataset.get_train_valid_samplers()
+    train_loader = DataLoader(
+        wt_dataset,
+        batch_size=cfg.training.batch_size,
+        sampler=train_sampler,
+        num_workers=cfg.training.num_workers,
+        drop_last=True,
+        pin_memory=True,
+    )
+    valid_loader = DataLoader(
+        wt_dataset,
+        batch_size=cfg.training.batch_size,
+        sampler=valid_sampler,
+        num_workers=cfg.training.num_workers,
+        pin_memory=True,
+        drop_last=False,
+    )
+
+    mdl = model.LitModel(
+        network=network.resnet18(
+            n_classes=wt_dataset.n_classes, base_channels=cfg.model.base_channels
+        ),
+        scale_factor=cfg.model.scale_factor,
+        seed=cfg.seed,
+        epochs=cfg.training.epochs,
+        lr=cfg.training.lr,
+        min_lr=cfg.training.min_lr,
+        metric_coefficient=cfg.model.metric_coefficient,
+    )
+
+    wandb_logger = WandbLogger(project=cfg.name)
+    wandb_logger.experiment.config.update(util.flatten_cfg(cfg))
+    tensorboard_logger = TensorBoardLogger(save_dir="tensorboard", name=cfg.name)
+    checkpoint_callback = ModelCheckpoint(
+        monitor="val_acc", mode="max", save_last=True, save_top_k=3
+    )
+
+    trainer = Trainer(
+        gpus=1,
+        max_epochs=cfg.training.epochs,
+        logger=[wandb_logger, tensorboard_logger],
+        callbacks=[checkpoint_callback],
+        fast_dev_run=cfg.dev,
+    )
+    trainer.fit(mdl, train_loader, valid_loader)
+
+    predictions, features, y = mdl.inference(valid_loader)
+
+    top_k = [1, 5, 10]
+    accuracy = {f"acc@top{k}": metrics.accuracy(y, predictions, top=k) for k in top_k}
+    for k, acc in accuracy.items():
+        print(f"{k}: {acc * 100: .2f}")
+    wandb_logger.log_metrics(accuracy)
+
+
+if __name__ == "__main__":
+    main()
